ea0f8f10a579cdf73c76e2a0802e7309668aa7b8
serdaruludag <uluserdar@gmail.com>
2022-08-09 23:08:45 +0300

    09.08.2022

---
+++ Stock.API/Consumer/OrderCreatedEventConsumer.cs
@@ -0,0 +1,20 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+using Shared;
+using Stock.API.Models;
+using System.Threading.Tasks;
+
+namespace Stock.API.Consumer
+{
+    public class OrderCreatedEventConsumer : IConsumer<OrderCreatedEvent>
+    {
+        private readonly AppDbContext _context;
+        private ILogger<OrderCreatedEventConsumer> _logger;
+        private readonly ISendEndpointProvider _sendEndpointProvider;
+
+        public Task Consume(ConsumeContext<OrderCreatedEvent> context)
+        {
+            
+        }
+    }
+}
